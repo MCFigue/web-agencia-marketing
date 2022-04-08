90ccb74279c9edaffee7948d0aa112cb36c2ad99
MCFigue <daniel.j.f.m@hotmail.com>
2022-04-08 17:45:21 -0500

    subida de boton scroll

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 
 import './App.css';
 import { Typewriter, useTypewriter, Cursor } from 'react-simple-typewriter'
+import { hover } from '@testing-library/user-event/dist/hover';
 
 
 function App() {
- 
+  
+  const scrollUp=()=>{
+    var currentScroll = document.documentElement.scrollTop
+    if(currentScroll > 0){
+      window.requestAnimationFrame(scrollUp);
+      window.scrollTo (0, currentScroll-(currentScroll/13));
+    }
+  }
+  const buttonUp = document.getElementById("button-up");
+ window.onscroll=function(){
+   var scroll = document.documentElement.scrollTop;
+   if(scroll > 100){
+     buttonUp.style.transform="scale(1)";
+   }else {
+     buttonUp.style.transform = "scale(0)"
+   }
+ }
 
   return (
     <>
+        <img className='w-14 fixed m-10 cursor-pointer hover:scale-110' style={{bottom:"0",right:"0"} } src='https://cdn.icon-icons.com/icons2/70/PNG/512/whatsapp_14158.png'></img>
+        <svg id='button-up' onClick={scrollUp} className='w-12 fixed m-12 mb-28 cursor-pointer hover:scale-110  bg-white shadow-lg  p-2 text-white' style={{bottom:"0",right:"0",color:"white",borderRadius:"49%",transform:"scale(1)"} }  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M416 352c-8.188 0-16.38-3.125-22.62-9.375L224 173.3l-169.4 169.4c-12.5 12.5-32.75 12.5-45.25 0s-12.5-32.75 0-45.25l192-192c12.5-12.5 32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25C432.4 348.9 424.2 352 416 352z" /></svg>
+
         <header className="text-white body-font  bg-gradient-to-r from-pink-700 via-pink-500 to-orange-400">
           <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center ">
               <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 " href="https://tailblocks.cc" target="_blank">
@@ -456,10 +476,10 @@ function App() {
               <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="inline-block w-8 h-8 text-pink-700" viewBox="0 0 975.036 975.036" >
                 <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
               </svg>
-              <p className="leading-relaxed text-lg">Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware. Man bun next level coloring book skateboard four loko knausgaard. Kitsch keffiyeh master cleanse direct trade indigo juice before they sold out gentrify plaid gastropub normcore XOXO 90's pickled cindigo jean shorts. Slow-carb next level shoindigoitch ethical authentic, yr scenester sriracha forage franzen organic drinking vinegar.</p>
+              <p className="leading-relaxed text-lg">Gracias a EsahMark por una buena gestion durante mi contrato con ellos, agradezco el progreso y exito logrado hasta el momento.Yo en particulas recomiendo este servicio : Plan de Servicio Pro</p>
               <span className="inline-block h-1 w-10 rounded bg-orange-400 mt-8 mb-6"></span>
-              <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">HOLDEN CAULFIELD</h2>
-              <p className="text-gray-500">Senior Product Designer</p>
+              <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">MEL MARCOS MOLLE</h2>
+              <p className="text-gray-500">CEO de EromeStudio</p>
             </div>
           </div>
         </section>
